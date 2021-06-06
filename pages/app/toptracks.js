a704441f41e6463b9063d15370f72b8051ad47a6
abh80 <techixspotify@gmail.com>
2021-06-06 10:00:07 +0530

    minor changes toptracks

--- pages/app/toptracks.js
+++ pages/app/toptracks.js
@@ -2,7 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { setTop } from "../../redux/actions/apiReducer";
 class TopTracks extends React.Component {
-    
+    render(){
+        return <h1>In development</h1>
+    }
 }
 
 export default connect((state) => state, { setTop })(TopTracks);
