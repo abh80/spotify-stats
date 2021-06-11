62f46a51f74dc37c54264619021f821bdf1a7798
abh80 <techixspotify@gmail.com>
2021-06-11 18:11:56 +0530

    Make comments to look cleaner and easier

--- apps/windows/preload.js
+++ apps/windows/preload.js
@@ -4,9 +4,13 @@ contextBridge.exposeInMainWorld("os", platform.os.toString());
 contextBridge.exposeInMainWorld("electron", true);
 const wrapper = {};
 contextBridge.exposeInMainWorld("wrapper", wrapper);
+
+// Event Handlers
 ipcRenderer.on("title-update", (e, title) => {
   document.getElementById("window-controls-title").textContent = title;
 });
+
+// Initialize window controls to work
 document.addEventListener("DOMContentLoaded", function DOMLOADED() {
   document.getElementById("close-btn").addEventListener("click", () => {
     ipcRenderer.invoke("close-window");
